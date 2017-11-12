a34407ff49cac4ddd323636418aa52c67bb7de16
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-11-12 23:30:24 +0100

    :memo: docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import './App.css';
-
+import { ButtonCounter } from './Components';
+import { User } from './Model/users';
+import { Menu } from './Components/Menu';
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+  users: User[] = [
+    {
+      name: 'Piotr',
+      age: 11
+    },
+    {
+      name: 'Kacper',
+      age: 2
+    }
+  ];
+  constructor(props: {}) {
+    super(props);
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(event: MouseEvent<HTMLButtonElement>) {
+    alert('clicked');
+  }
   render() {
     return (
       <div className="App">
@@ -14,6 +34,10 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          <ButtonCounter />
+        </p>
+        <Menu />
       </div>
     );
   }
--- src/Model/users.ts
+++ src/Model/users.ts
@@ -1,8 +1,22 @@
+/**
+ * User contract
+ * @export
+ * @interface User
+ */
 export interface User {
   name: string;
   age: number;
 }
 
+/**
+ * Users collection
+ * @export
+ * @interface Users
+ * @extends {Array<User>}
+ */
 export interface Users extends Array<User> {}
 
+/**
+ * Type for fetch users callback function
+ */
 export type FetchUsersHandler = (users: Users) => void;
